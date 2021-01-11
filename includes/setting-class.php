89b5ea29f1c84d053afb7ec6cd9cca5e34b976b2
ALi BACHIR <alibasheer@hotmail.com>
2021-01-11 22:02:52 +0200

    code style edits to Setting class

--- includes/setting-class.php
+++ includes/setting-class.php
@@ -1,36 +1,29 @@
 <?php
 
-class WC_Settings_Tab_API
-{
+class WC_Settings_Tab_LibanPost_API {
 
-	public static function init()
-	{
+	public static function init() {
 		add_filter('woocommerce_settings_tabs_array', __CLASS__ . '::add_settings_tab', 50);
 		add_action('woocommerce_settings_tabs_settings_tab_api', __CLASS__ . '::settings_tab');
 		add_action('woocommerce_update_options_settings_tab_api', __CLASS__ . '::update_settings');
 	}
 
-
-	public static function add_settings_tab($settings_tabs)
-	{
+	public static function add_settings_tab($settings_tabs) {
 		$settings_tabs['settings_tab_api'] = __('LibanPost API', 'woocommerce-settings-tab-api');
 		return $settings_tabs;
 	}
 
 
-	public static function settings_tab()
-	{
+	public static function settings_tab() {
 		woocommerce_admin_fields(self::get_settings());
 	}
 
 
-	public static function update_settings()
-	{
+	public static function update_settings() {
 		woocommerce_update_options(self::get_settings());
 	}
 
-	public static function get_settings()
-	{
+	public static function get_settings() {
 
 		$settings = array(
 			'section_title' => array(
@@ -71,7 +64,6 @@ class WC_Settings_Tab_API
 
 		return apply_filters('wc_settings_tab_api_settings', $settings);
 	}
-
 }
 
-WC_Settings_Tab_API::init();
+WC_Settings_Tab_LibanPost_API::init();
