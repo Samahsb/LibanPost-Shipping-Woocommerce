89fd14b2b6c35daf7167120fad3f418ee31662b7
ALi BACHIR <alibasheer@hotmail.com>
2021-01-11 21:58:50 +0200

    move plugin settings to stand alone file

--- libanpost.php
+++ libanpost.php
@@ -10,6 +10,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+include_once dirname( __FILE__ ) . '/includes/setting-class.php';
+
 /**
  * Adds plugin page configure link
  */
@@ -21,81 +23,6 @@ function libanpost_shipping_plugin_links( $links ) {
 }
 add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'libanpost_shipping_plugin_links' );
 
-class WC_Settings_Tab_API
-{
-
-    public static function init()
-    {
-        add_filter('woocommerce_settings_tabs_array', __CLASS__ . '::add_settings_tab', 50);
-        add_action('woocommerce_settings_tabs_settings_tab_api', __CLASS__ . '::settings_tab');
-        add_action('woocommerce_update_options_settings_tab_api', __CLASS__ . '::update_settings');
-    }
-
-
-    public static function add_settings_tab($settings_tabs)
-    {
-        $settings_tabs['settings_tab_api'] = __('LibanPost API', 'woocommerce-settings-tab-api');
-        return $settings_tabs;
-    }
-
-
-    public static function settings_tab()
-    {
-        woocommerce_admin_fields(self::get_settings());
-    }
-
-
-    public static function update_settings()
-    {
-        woocommerce_update_options(self::get_settings());
-    }
-
-    public static function get_settings()
-    {
-
-        $settings = array(
-            'section_title' => array(
-                'name' => __('API integration', 'woocommerce-settings-tab-api'),
-                'type' => 'title',
-                'desc' => '',
-                'id' => 'wc_settings_tab_api_section_title'
-            ),
-            'title' => array(
-                'name' => __('Title', 'woocommerce-settings-tab-api'),
-                'type' => 'text',
-                'desc' => __('Title of your store', 'woocommerce-settings-tab-api'),
-                'id' => 'wc_settings_tab_api_title'
-            ),
-            'email' => array(
-                'name' => __('Email', 'woocommerce-settings-tab-api'),
-                'type' => 'email',
-                'desc' => __('Email address for your site.', 'woocommerce-settings-tab-api'),
-                'id' => 'wc_settings_tab_email'
-            ),
-            'api' => array(
-                'name' => __('API key', 'woocommerce-settings-tab-api'),
-                'type' => 'text',
-                'desc' => __('The API key for connecting with your LibanPost account.', 'woocommerce-settings-tab-api'),
-                'id' => 'wc_settings_tab_api'
-            ),
-            'erpcode' => array(
-                'name' => __('ERPCode', 'woocommerce-settings-tab-api'),
-                'type' => 'text',
-                'desc' => __('The ERPCode for connecting with your LibanPost account.', 'woocommerce-settings-tab-api'),
-                'id' => 'wc_settings_tab_erpcode'
-            ),
-            'section_end' => array(
-                'type' => 'sectionend',
-                'id' => 'wc_settings_tab_api_section_end'
-            )
-        );
-
-        return apply_filters('wc_settings_tab_api_settings', $settings);
-    }
-
-}
-
-WC_Settings_Tab_API::init();
 
 //enqueue js and css files
 
