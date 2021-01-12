72c7cb97adcff3f0ef2ef2629d2ca60c3380820e
Samah <samahssb2001@gmail.com>
2021-01-12 20:03:38 +0200

    add dashicon rotate icon

--- includes/prepare-shipment.php
+++ includes/prepare-shipment.php
@@ -18,7 +18,7 @@ function libanpost_woocommerce_admin_order_data_after_order_details( $wccm_befor
                 <input type="text">
             </fieldset>
             <div class="create-shipment" onclick="libanPostAJAXRequest()">
-                <div class="ajax-response">test</div>
+                <div class="ajax-response"> <span class="dashicons dashicons-image-rotate"></span> </div>
                 <div class="libanpost-btn"> Create Shipment </div>
             </div>
 		</div>
