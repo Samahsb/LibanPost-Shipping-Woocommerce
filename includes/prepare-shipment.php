f43bdcbe3986cfc0bab7d4242ac161d8086abbac
Samah <samahssb2001@gmail.com>
2021-01-14 12:19:16 +0200

    added fieldset inputs and styling them

--- includes/prepare-shipment.php
+++ includes/prepare-shipment.php
@@ -9,13 +9,142 @@ function libanpost_woocommerce_admin_order_data_after_order_details( $wccm_befor
 	<div class="libanpost-overlay" id="libanpost_overlay">
 		<div class="libanpost-shipment-creation">
 			<span class="dashicons dashicons-no-alt" onclick="hideShipmentDetails()"></span>
-			<fieldset>
-				<legend>Billing Account</legend>
-				<input type="text">
-			</fieldset>
-            <fieldset>
-                <legend>Billing Account</legend>
-                <input type="text">
+            <fieldset class="libanpost-fieldset">
+                <legend>PK Order</legend>
+                <div>
+                    <label>PERSON FID</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label>ORDER SPEED ID</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label>ORDER TYPE ID</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label>ORDER ENTITY ID</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label>REFERENCE ID</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label>ESTIMATED NO OF ITEMS</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label>ESTIMATED WEIGHT</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label>ENTRY DATE</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label>EVT GMT DT</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label>EVT TRACKING NO DECD</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label>ORDER OCCURENCE ID</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label>ORDER DATE</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label>ORDER STATUS</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label>NOTIFICATION TYPE CD</label>
+                    <input type="text">
+                </div>
+            </fieldset>
+            <fieldset class="libanpost-fieldset">
+                <legend>PK Order Details</legend>
+                <div>
+                    <label>REFERENCE NO</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label>DEPOSITOR FULLNAME</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label>DEPOSITOR ADDRESS</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label>DEPOSITOR PHONE NO</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label>CLIENT FULLNAME</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label>CLIENT ADDRESS</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label>CLIENT PHONENO</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label>ESTIMATED WEIGHT</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label>VEHICLE TYPE ID</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label>PREF VISIT DATE FROM</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label>PREF VISIT DATE TO</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label>EVTGMT DT</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label>ITEM DESC</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label>Notes</label>
+                    <input type="text">
+                </div>
+            </fieldset>
+            <fieldset class="libanpost-fieldset">
+                <legend>PK Order Details Charges</legend>
+                <div>
+                    <label>FEES ID</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label>CURRENCY CD</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label>AMOUNT</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label>PAYMENT MODE ID</label>
+                    <input type="text">
+                </div>
             </fieldset>
             <div class="create-shipment">
                 <div class="libanpost-ajax-response">
@@ -27,5 +156,7 @@ function libanpost_woocommerce_admin_order_data_after_order_details( $wccm_befor
 		</div>
 	</div>
 	<?php
+//    $order = wc_get_order();
+//    var_dump($order->get_billing_first_name());
 }
 add_action( 'woocommerce_admin_order_data_after_order_details', 'libanpost_woocommerce_admin_order_data_after_order_details', 10, 1 );
