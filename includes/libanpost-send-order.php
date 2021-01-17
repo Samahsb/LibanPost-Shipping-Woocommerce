547c4cac8c900e692945d270657d96b00a659f34
ALi BACHIR <alibasheer@hotmail.com>
2021-01-17 13:12:34 +0200

    send post request

--- includes/libanpost-send-order.php
+++ includes/libanpost-send-order.php
@@ -1,6 +1,25 @@
 <?php
 
 function libanpost_send_order() {
-	wp_send_json('yes');
+
+	$request_data = $_POST['orderData'];
+	wp_send_json($request_data);
+	$request_url = "https://hemi.Libanpost.com/api/PKOrder?token=Token_Given&ERPCode=ERP";
+
+	// Request the session
+	$response_json = wp_remote_post( $request_url, array(
+		'body'	  => json_encode ( $request_data ),
+//		'headers' => array(
+//			'Authorization' => 'Basic ' . base64_encode( "merchant." . $this->merchant_id . ":" . $this->auth_pass ),
+//		),
+	) );
+
+	if ( is_wp_error( $response_json ) ) {
+		wp_send_json('WordPress failed to connect with libanpost server');
+	}
+
+	$response = json_decode( $response_json['body'], true );
+
+	wp_send_json($response);
 }
 add_action( 'wp_ajax_libanpost_send_order', 'libanpost_send_order' );
