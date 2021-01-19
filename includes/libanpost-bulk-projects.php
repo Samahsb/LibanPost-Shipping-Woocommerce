79eaab0b646a856f1d77478386b7b752e67e7ab2
Samah <samahssb2001@gmail.com>
2021-01-19 23:14:06 +0200

    change the names of the submenu pages

--- includes/libanpost-bulk-projects.php
+++ includes/libanpost-bulk-projects.php
@@ -2,32 +2,32 @@
 /**
  * register LibanPost projects page
  */
-function libanpost_register_project_page() {
-    add_submenu_page( 'woocommerce', 'LibanPost Projects', 'LibanPost Projects', 'manage_options', 'libanpost-projects', 'libanpost_project_page_callback', 2 );
+function libanpost_register_projects_list_page() {
+    add_submenu_page( 'woocommerce', 'LibanPost Projects List', 'LibanPost Projects List', 'manage_options', 'libanpost-projects-list', 'libanpost_project_page_callback', 2 );
 }
 function libanpost_project_page_callback() {
     ?>
     <div class="wrap">
-        <h1 class="wp-heading-inline libanpost-heading">LibanPost Projects</h1>
-        <a href="<?php echo admin_url('admin.php?page=libanpost-bulk-orders'); ?>" class="page-title-action">Send LibanPost Bulk Orders</a>
+        <h1 class="wp-heading-inline libanpost-heading">LibanPost Projects List</h1>
+        <a href="<?php echo admin_url('admin.php?page=submit-libanpost-project'); ?>" class="page-title-action">Send LibanPost Project</a>
     </div>
     <?php
     $url = admin_url('admin.php?page=libanpost-projects');
 }
-add_action('admin_menu', 'libanpost_register_project_page',99);
+add_action('admin_menu', 'libanpost_register_projects_list_page',99);
 
 /**
  * register LibanPost bulk orders page
  */
-function libanpost_register_bulk_orders_page() {
-    add_submenu_page( 'woocommerce', 'LibanPost Bulk Orders', 'LibanPost Bulk Orders', 'manage_options', 'libanpost-bulk-orders', 'libanpost_bulk_orders_page_callback', 3 );
+function libanpost_register_submit_project_page() {
+    add_submenu_page( 'woocommerce', 'Submit LibanPost Project', 'Submit LibanPost Project', 'manage_options', 'submit-libanpost-project', 'libanpost_submit_project_page_callback', 3 );
 }
-function libanpost_bulk_orders_page_callback() {
+function libanpost_submit_project_page_callback() {
     ?>
     <div class="wrap">
-        <h1 class="wp-heading-inline libanpost-heading">LibanPost Bulk Orders</h1>
-        <a href="#" class="page-title-action">Send LibanPost Bulk Orders</a>
+        <h1 class="wp-heading-inline libanpost-heading">Submit LibanPost Project</h1>
+        <a href="#" class="page-title-action">Submit LibanPost Project</a>
     </div>
     <?php
 }
-add_action('admin_menu', 'libanpost_register_bulk_orders_page',99);
+add_action('admin_menu', 'libanpost_register_submit_project_page',99);
