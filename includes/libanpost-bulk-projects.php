cc32f40c388cd3e010b76c27c06a933644d9fd16
Samah <samahssb2001@gmail.com>
2021-01-19 23:02:31 +0200

    added woocommerce submenu pages for bulk orders and projects

--- includes/libanpost-bulk-projects.php
+++ includes/libanpost-bulk-projects.php
@@ -1,33 +1,33 @@
 <?php
 /**
- * register LibanPost submenu page
+ * register LibanPost projects page
  */
-function libanpost_register_submenu_page() {
-    add_submenu_page( 'woocommerce', 'LibanPost Projects', 'LibanPost Projects', 'manage_options', 'libanpost-submenu-page', 'libanpost_submenu_page_callback', 2 );
+function libanpost_register_project_page() {
+    add_submenu_page( 'woocommerce', 'LibanPost Projects', 'LibanPost Projects', 'manage_options', 'libanpost-projects', 'libanpost_project_page_callback', 2 );
 }
-function libanpost_submenu_page_callback() {
+function libanpost_project_page_callback() {
     ?>
     <div class="wrap">
-        <h1 class="wp-heading-inline libanpost-heading">LibanPost Bulk Projects</h1>
-        <a href="#" class="page-title-action">Send LibanPost Bulk Orders</a>
+        <h1 class="wp-heading-inline libanpost-heading">LibanPost Projects</h1>
+        <a href="<?php echo admin_url('admin.php?page=libanpost-bulk-orders'); ?>" class="page-title-action">Send LibanPost Bulk Orders</a>
     </div>
     <?php
-    $url = admin_url('admin.php?page=libanpost-submenu-page');
+    $url = admin_url('admin.php?page=libanpost-projects');
 }
-add_action('admin_menu', 'libanpost_register_submenu_page',99);
+add_action('admin_menu', 'libanpost_register_project_page',99);
 
 /**
- * add LibanPost submenu page
+ * register LibanPost bulk orders page
  */
-function libanpost_add_submenu_page() {
-    add_submenu_page( 'libanpost-submenu-page', 'LibanPost add Projects', 'LibanPost add Projects', 'manage_options', 'libanpost-add-submenu-page', 'libanpost_add_submenu_page_callback', 2 );
+function libanpost_register_bulk_orders_page() {
+    add_submenu_page( 'woocommerce', 'LibanPost Bulk Orders', 'LibanPost Bulk Orders', 'manage_options', 'libanpost-bulk-orders', 'libanpost_bulk_orders_page_callback', 3 );
 }
-function libanpost_add_submenu_page_callback() {
+function libanpost_bulk_orders_page_callback() {
     ?>
     <div class="wrap">
-        <h1 class="wp-heading-inline libanpost-heading">LibanPost Bulk Projects</h1>
+        <h1 class="wp-heading-inline libanpost-heading">LibanPost Bulk Orders</h1>
         <a href="#" class="page-title-action">Send LibanPost Bulk Orders</a>
     </div>
     <?php
 }
-add_action('admin_menu', 'libanpost_add_submenu_page',99);
+add_action('admin_menu', 'libanpost_register_bulk_orders_page',99);
