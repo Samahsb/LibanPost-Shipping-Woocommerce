de594f3525e9a4c38e887553ce0deea2d3a3a1f5
ALi BACHIR <alibasheer@hotmail.com>
2021-02-05 18:58:32 +0200

    include barcode library

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'a1e9ef8fa85de7f98ffc0f9376b8d7ffe637d168',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a1e9ef8fa85de7f98ffc0f9376b8d7ffe637d168',
+    ),
+    'picqer/php-barcode-generator' => 
+    array (
+      'pretty_version' => 'v2.1.0',
+      'version' => '2.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2221d834efab0dbfa9d37f9dfb4673d7f27f3757',
+    ),
+  ),
+);
